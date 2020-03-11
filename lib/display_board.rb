82c9e6cf5994490d339c8ee822b539b5c606f01a
mathematical-integer-7084 <shawnaejohnson6@gmail.com>
2020-03-11 09:17:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,11 @@
 def display_board
   puts "   |   |   "
+<<<<<<< HEAD
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
+=======
+  puts "   |   |   "
+>>>>>>> bd004690305e61de030f96a331c9fc94076d7b16
   puts "   |   |   "
 end
